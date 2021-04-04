fe671118992d499809860618f407713397d97474
ashgray <7ashgray7@gmail.com>
2021-04-04 12:44:48 +0900

    ユメグラチケット情報サイト フロントエンド実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { castInfo } from "./consts/castInfo";
+import "./App.css";
+import "./Cast.css";
+import "./Choose.css";
+import axios from "axios";
+import dayjs from "dayjs"
+
+export interface Response {
+  tweets: {
+    id: string;
+    text: string;
+    author_id: string;
+    created_at: string; // ISO string
+  }[];
+  cast: {
+    id: string;
+    name: string;
+    username: string;
+    profile_image_url: string;
+  };
+}
 
 function App() {
+  const [requestIds, setId] = useState([] as string[]);
+  const [castData, setTweetObject] = useState([] as Response[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="App-header">ユメグラ非公式チケット情報サイト</header>
+      <div style={{ marginRight: 300, marginLeft: 300 }}>
+        {castInfo.map((data) => {
+          if (!requestIds.includes(data.castId)) {
+            return (
+              <a
+                className={`btn effect${data.castId}`}
+                onClick={() => {
+                  let arr;
+                  if (requestIds.includes(data.castId)) {
+                    arr = requestIds.filter((id) => id !== data.castId);
+                  } else {
+                    arr = [...requestIds, data.castId];
+                  }
+                  setId(arr);
+                }}
+              >
+                {data.name}
+              </a>
+            );
+          } else {
+            return (
+              <a
+                className={`btn-choose${data.castId} effect-choose${data.castId}`}
+                onClick={() => {
+                  let arr;
+                  if (requestIds.includes(data.castId)) {
+                    arr = requestIds.filter((id) => id !== data.castId);
+                  } else {
+                    arr = [...requestIds, data.castId];
+                  }
+                  setId(arr);
+                }}
+              >
+                {data.name}
+              </a>
+            );
+          }
+        })}
+      </div>
+      <div
+        style={{
+          marginRight: 300,
+          marginLeft: 300,
+          marginTop: 100,
+          marginBottom: 200,
+        }}
+      >
+        <button
+          disabled={requestIds.length === 0}
+          onClick={async () => await fetchTweet(requestIds, setTweetObject, setId)}
         >
-          Learn React
-        </a>
-      </header>
+          {requestIds.length === 0
+            ? "チケット情報を取得するキャストさんを3名まで選択してください"
+            : "チケット情報を取得する"}
+        </button>
+      </div>
+      {castData && (
+        <div>
+          {castData.map((cast) => {
+            return (
+              <div style={{display: "inline-grid", marginRight: 100}}>
+                <div>
+                <img src={cast.cast.profile_image_url} />
+                <div style={{fontSize: 20, paddingBottom: 20}}>
+                  {cast.cast.name}
+                </div>
+                </div>
+                {cast.tweets.map((tweet) => (
+                  <div style ={{width: 340, marginBottom: 30, fontSize: 16}}>
+                    {tweet.text}
+                    <br /><br />
+                    ツイート日時: {dayjs(tweet.created_at).format("YYYY[年]MM[月]DD[日] HH[時]mm[分]ss[秒]")}
+                    <div style={{ marginTop: 20,borderTop: "1px", borderLeft: "1px", borderRight: "1px", borderColor:"#dbdbdb", borderStyle:'solid'}} />
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
+}
+
+async function fetchTweet(
+  requestIds: string[],
+  setTweetObject: React.Dispatch<React.SetStateAction<Response[]>>,
+  setId: React.Dispatch<React.SetStateAction<string[]>>
+) {
+  console.log("request", requestIds);
+  console.log("request", JSON.stringify({ requestIds: requestIds }));
+  const response = await axios.post(
+    "https://1t9s9wq59b.execute-api.us-east-1.amazonaws.com/develop/ticket",
+    JSON.stringify({ requestIds: requestIds })
+  );
+  console.log("api", response);
+  setTweetObject(response.data.content);
+  setId([])
+  return;
 }
 
 export default App;
---
+++ src/consts/castInfo.ts
@@ -0,0 +1,134 @@
+export const castInfo = [
+    {
+        name: "南　絢音",
+        castId: "2",
+    },
+    {
+        name: "聖 グレイル",
+        castId: "3",
+    },
+    {
+        name: "宝生　てあ",
+        castId: "4",
+    },
+    {
+        name: "六道　カロ",
+        castId: "5",
+    },
+    {
+        name: "鳩岡　小恋",
+        castId: "9",
+    },
+    {
+        name: "ころぼし　まう",
+        castId: "10",
+    },
+    {
+        name: "御影　みゆ",
+        castId: "11",
+    },
+    {
+        name: "筆染　四季",
+        castId: "12",
+    },
+    {
+        name: "雎　雪待",
+        castId: "13",
+    },
+    {
+        name: "苺ノ森　こひな",
+        castId: "14",
+    },
+    {
+        name: "小花　てくと",
+        castId: "15",
+    },
+    {
+        name: "りいか",
+        castId: "16",
+    },
+    {
+        name: "四ノ宮 透華",
+        castId: "17",
+    },
+    {
+        name: "ロッド・クロウリー",
+        castId: "18",
+    },
+    {
+        name: "紅夜　ぺい",
+        castId: "19",
+    },
+    {
+        name: "蝿取　かづら",
+        castId: "20",
+    },
+    {
+        name: "ミーティア",
+        castId: "21",
+    },
+    {
+        name: "朝霧　いづる",
+        castId: "22",
+    },
+    {
+        name: "暮間　珀暮",
+        castId: "23",
+    },
+    {
+        name: "蒼海　カヤナ",
+        castId: "24",
+    },
+    {
+        name: "雛芽　るち",
+        castId: "25",
+    },
+    {
+        name: "福寿　礼",
+        castId: "26",
+    },
+    {
+        name: "海老名　ラムネ",
+        castId: "27",
+    },
+    {
+        name: "園田　小陽",
+        castId: "28",
+    },
+    {
+        name: "御仲　いのり",
+        castId: "29",
+    },
+    {
+        name: "天矢　のの",
+        castId: "30",
+    },
+    {
+        name: "信濃　奈乃",
+        castId: "31",
+    },
+    {
+        name: "朱華　うる",
+        castId: "32",
+    },
+    {
+        name: "杠　芽里",
+        castId: "33",
+    },
+    {
+        name: "明燈　みちか",
+        castId: "34",
+    },
+    {
+        name: "池鯉鮒 ももり",
+        castId: "35",
+    },
+    {
+        name: "檸檬 かふる",
+        castId: "36",
+    },
+    {
+        name: "帆孝 清澄",
+        castId: "37",
+    },
+]
